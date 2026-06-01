622165f4f0f3cc2929db80d5a7ba62cdcfb56a39
AlanConsorte <alanfconsorte@gmail.com>
2026-06-01 20:42:56 -0300

    Revert "fazendo alguns html"
    This reverts commit 0722390e82c6966c55b0d07cf92986dfa60c51ef.

--- src/main/java/br/edu/utfpr/td/tsi/agencia/noticias/persistencia/NoticiaRepository.java
+++ src/main/java/br/edu/utfpr/td/tsi/agencia/noticias/persistencia/NoticiaRepository.java
@@ -3,19 +3,10 @@ package br.edu.utfpr.td.tsi.agencia.noticias.persistencia;
 import java.time.LocalDate;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
-import br.edu.utfpr.td.tsi.agencia.noticias.modelo.Assunto;
-import br.edu.utfpr.td.tsi.agencia.noticias.modelo.Autor;
 import br.edu.utfpr.td.tsi.agencia.noticias.modelo.Noticia;
-import br.edu.utfpr.td.tsi.agencia.noticias.modelo.SituacaoNoticia;
 
 public interface NoticiaRepository extends MongoRepository<Noticia, String> {
 
     long countByAutorIdAndAssuntoRelacionadoIdAndDataCriacao(String autorId, String assuntoId, LocalDate dataCriacao);
-
-    Noticia findAllByAutor(Autor autor);
-
-    Noticia findAllByAssunto(Assunto assunto);
-
-    Noticia findAllbysituacaoNoticia(SituacaoNoticia situacaoNoticia);
 
 }
